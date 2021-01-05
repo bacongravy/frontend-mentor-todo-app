1daebe82019b1749fbebfbaa6fc31e1689ca46f5
David Kramer <davidkramer@me.com>
2021-01-05 13:18:45 -0800

    Remove unnecessary box

--- components/TodoBackground.js
+++ components/TodoBackground.js
@@ -1,22 +1,21 @@
 import { Box, useColorMode, useColorModeValue } from "@chakra-ui/react";
 
 const TodoBackground = (props) => {
-    const { colorMode } = useColorMode();
-    return (
-      <Box bgColor={useColorModeValue("light.100", "dark.900")}>
-        <Box
-          minH="100vh"
-          minW="100vw"
-          bgImage={{
-            base: `url('/bg-mobile-${colorMode}.jpg')`,
-            sm: `url('/bg-desktop-${colorMode}.jpg')`,
-          }}
-          bgRepeat="no-repeat"
-          bgSize="100vw"
-          {...props}
-        ></Box>
-      </Box>
-    );
-  };
-  
-  export default TodoBackground;
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      minH="100vh"
+      minW="100vw"
+      bgColor={useColorModeValue("light.100", "dark.900")}
+      bgImage={{
+        base: `url('/bg-mobile-${colorMode}.jpg')`,
+        sm: `url('/bg-desktop-${colorMode}.jpg')`,
+      }}
+      bgRepeat="no-repeat"
+      bgSize="100vw"
+      {...props}
+    />
+  );
+};
+
+export default TodoBackground;
